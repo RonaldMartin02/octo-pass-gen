6ee2d905ebac9a3ed472e1c4859283a6504ec98c
Ronald Martin <ronaldmartin02@gmail.com>
2023-10-03 22:17:39 -0400

    added return statments to improve fuctionality

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -19,55 +19,52 @@ function writePassword() {
 
 function generatePassword() {
   var password = "";
-  var passArray = [];
+  var passArray = []; 
+  // Allows for the prompt to ///
   var pLength = parseInt(prompt("How long do you want your pasword to be: \n (Please enter only numbers between 8-128)"));
+   if(!pLength)return "";
+   
   if (pLength >= 8 && pLength <= 128) {
     passArray = passArray.concat(lCaseArray)
     var uCaseConfirm = confirm("Do you need a Capital letters:");
     if (uCaseConfirm) {
       passArray = passArray.concat(uCaseArray);
-      alert("OK")
-    } else {
-      alert("No")
     }
     var numConfirm = confirm("Do you need Numbers letters:");
     if (numConfirm) {
       passArray = passArray.concat(numArray)
-      alert("OK")
-    } else {
-      alert("No")
     }
     var specConfirm = confirm("Do you need Special Characters:");
     if (specConfirm) {
       passArray = passArray.concat(specArray)
-      alert("OK")
-    } else {
-      alert("No")
     }
     for (var i = 0; i < pLength; i++) {
-      // This statement will run each time the loop is executed
-      // console.log(passArray[i]);
       password += passArray[Math.floor(Math.random() * passArray.length)]
-      
     }
     return password;
   } else if (pLength < 8) {
     alert("Too Short")
-    var uChoice = confirm("would you like to try again");
+    var uChoice = confirm("Do you want to try again");
     if (uChoice) {
       return generatePassword();
+    } else {
+      return "";
     }
   } else if (pLength > 128) {
     alert("Too Long")
-    var uChoice = confirm("would you like to try again");
+    var uChoice = confirm("Do you want to try again");
     if (uChoice) {
       return generatePassword();
+    } else {
+      return "";
     }
   } else {
     alert("Please enter only numbers between 8-128");
-    var uChoice = confirm("would you like to try again");
+    var uChoice = confirm("Do you want to try again");
     if (uChoice) {
       return generatePassword();
+    } else {
+      return "";
     }
   }
 }
